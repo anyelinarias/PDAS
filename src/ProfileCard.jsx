39f9f0fe65ad85918af4624d6ee10a611c694eb3
anyelinarias <anyelinarias899@gmail.com>
2025-03-27 18:16:32 -0700

    Cambio ligero en ProfileCard

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,6 +1,9 @@
-function ProfileCard() {
+function ProfileCard({title, handle, image}) {
+    console.log(title)
+    console.log(handle)
+    console.log(image)
   return (
-    <div>ProfileCard</div>
+    <div>ProfileCard2</div>
   )
 }
 
